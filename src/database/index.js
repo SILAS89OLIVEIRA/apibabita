769d877f05623a2a331a1137c3e4ab86aafa05d3
silas89oliveira <silas.henrique@hotmail.com>
2020-07-24 15:17:42 -0300

    login e venda/estoque

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,40 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
-const erp_usuario = require('../models/Usuarios');
+
+
+const vendas = require('../models/Vendas');
+
+const estoquetotal = require('../models/Estoquetotal');
+
+const erp_usuarios = require('../models/erp_usuario');
+
+
+const erp_fornecedor_usuario = require('../models/erp_fornecedor_usuario');
+
+
+
+
 
 const connection = new Sequelize(dbConfig);
 
-erp_usuario.init(connection);
+
+vendas.init(connection);
+
+estoquetotal.init(connection);
+
+erp_usuarios.init(connection);
+
+erp_fornecedor_usuario.init(connection);
+
+
+
+
+
+
+erp_fornecedor_usuario.associate(connection.models);
+
+erp_usuarios.associate(connection.models);
 
 
 
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,36 @@
 const express = require('express');
 
-const UserController = require('./controllers/UserController');
+const VendaController = require('./controllers/VendaController');
+
+const VendadiaController = require('./controllers/VendadiaController');
+
+const VendamesController = require('./controllers/VendamesController');
+
+const EstoquetotalController = require('./controllers/EstoquetotalController');
+
+const UsuariosController = require('./controllers/UsuariosController');
+
+const UsuFornController = require('./controllers/UsuFornController');
+
+
+
 
 
 const routes = express.Router();
 
-routes.get('/usuarios', UserController.index);
-routes.post('/usuarios', UserController.store);
-routes.delete('/usuarios/:usu_cod', UserController.delete);
-//routes.put('/usuarios', UserController.store);
+
+
+
+routes.get('/Vendas/:for_cod/:data/:data2',VendaController.index);
+
+routes.get('/Vendasdia/:for_cod/:data',VendadiaController.index);
+
+routes.get('/Vendasmes/:for_cod/:data/:data2',VendamesController.index);
+
+routes.get('/Estoquetotal/:for_cod',EstoquetotalController.index);
+
+routes.get('/erp_usuarios/:usu_login/:usu_senha',UsuariosController.index);
+
 
 
 
